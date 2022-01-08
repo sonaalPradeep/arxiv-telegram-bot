55da65042fd725a194bcb8c66a1ae37fa2d093e3
Sonaal Pathlai Pradeep <unofficial.sonaal@gmail.com>
2022-01-08 19:19:18 +0530

    Manually linted files

--- arxiv_telegram_bot/__init__.py
+++ arxiv_telegram_bot/__init__.py
@@ -2,6 +2,7 @@ from decouple import config
 import requests
 from bs4 import BeautifulSoup
 import lxml
+
 __version__ = "0.1.0"
 response = requests.get("http://export.arxiv.org/api/query?search_query=all:electron")
 xml_data = BeautifulSoup(response.text, "lxml")
