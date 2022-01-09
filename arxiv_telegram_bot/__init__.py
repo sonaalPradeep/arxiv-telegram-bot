2dd6089dba8d6bab4c1563e1de4c9bd335b039b3
Sonaal Pathlai Pradeep <unofficial.sonaal@gmail.com>
2022-01-09 11:43:33 +0530

    Testing with CS categories

--- arxiv_telegram_bot/__init__.py
+++ arxiv_telegram_bot/__init__.py
@@ -4,6 +4,6 @@ from bs4 import BeautifulSoup
 import lxml
 
 __version__ = "0.1.0"
-response = requests.get("http://export.arxiv.org/api/query?search_query=all:electron")
+response = requests.get("https://export.arxiv.org/api/query?search_query=cat:cs.CV+OR+cat:cs.LG+OR+cat:cs.CL+OR+cat:cs.AI+OR+cat:cs.NE+OR+cat:cs.RO")
 xml_data = BeautifulSoup(response.text, "lxml")
 print(xml_data.find_all("entry")[0])
