1506812b3f5ccc8b60085d365be6ed79c0903393
Goutham Krishna <gauthamkrishna9991@live.com>
2022-01-14 19:39:31 +0530

    feat: Convert current application into module

--- arxiv_telegram_bot/__init__.py
+++ arxiv_telegram_bot/__init__.py
@@ -1,20 +1,4 @@
-from decouple import config
-import arxiv
-from loguru import logger
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-__version__ = "0.1.0"
-
-query_catalogues = ["cs.CV", "cs.LG", "cs.CL", "cs.AI", "cs.NE", "cs.RO"]
-query_string = " OR ".join(query_catalogues)
-
-search = arxiv.Search(
-    query=query_string,
-    max_results=100,
-    sort_by=arxiv.SortCriterion.SubmittedDate,
-    sort_order=arxiv.SortOrder.Descending,
-)
-
-for result in search.results():
-    print(
-        f"{result.title} - {result.published}\n{result.categories}\n{result.summary}\n\n"
-    )
+__version__ = "0.1.0" 
