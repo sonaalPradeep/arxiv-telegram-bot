1b15844f3ec0143722ba19d4191b0354b2fe81b6
Goutham Krishna <gauthamkrishna9991@live.com>
2022-01-14 20:04:58 +0530

    feat!: Build a polling-based telegram app.

--- arxiv_telegram_bot/__main__.py
+++ arxiv_telegram_bot/__main__.py
@@ -2,24 +2,31 @@
 # -*- coding: utf-8 -*-
 
 """
-Arxiv Telegram Bot Module
+# Arxiv Telegram Bot Module
 
 Main Module for Arxiv
 """
 
-import arxiv
+"""# import arxiv
 
-query_catalogues = ["cs.CV", "cs.LG", "cs.CL", "cs.AI", "cs.NE", "cs.RO"]
-query_string = " OR ".join(query_catalogues)
+# query_catalogues = ["cs.CV", "cs.LG", "cs.CL", "cs.AI", "cs.NE", "cs.RO"]
+# query_string = " OR ".join(query_catalogues)
 
-search = arxiv.Search(
-    query=query_string,
-    max_results=100,
-    sort_by=arxiv.SortCriterion.SubmittedDate,
-    sort_order=arxiv.SortOrder.Descending,
-)
+# search = arxiv.Search(
+#     query=query_string,
+#     max_results=100,
+#     sort_by=arxiv.SortCriterion.SubmittedDate,
+#     sort_order=arxiv.SortOrder.Descending,
+# )
 
-for result in search.results():
-    print(
-        f"{result.title} - {result.published}\n{result.categories}\n{result.summary}\n\n"
-    )
+# for result in search.results():
+#     print(
+#         f"{result.title} - {result.published}\n{result.categories}\n{result.summary}\n\n"
+#     )
+"""
+
+from arxiv_telegram_bot.base import main
+
+
+if __name__ == "__main__":
+    main()
---
+++ arxiv_telegram_bot/base.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+# Arxiv Base Imports
+
+
+"""
+
+import logging
+from setuptools import Command
+
+from telegram.ext import Updater, CommandHandler, Dispatcher, MessageHandler, Filters, CallbackContext
+from telegram import Update
+import os
+
+port = int(os.environ.get("PORT", 5000))
+
+# enable logging
+logger = logging.getLogger(__name__)
+
+TOKEN = "5057319565:AAEX2dcT93FJ4AToOZlrVhvuPxJy051c_nQ"
+
+# Define a few command handlers
+
+def start(update: Update, context: CallbackContext):
+    """Send a message when the command 'start' is issued."""
+    print(context._user_id_and_data[0])
+    update.message.reply_text("Hi! Your userid")
+
+def uid(update: Update, context: CallbackContext):
+    """Ping back the userid whose command created"""
+    update.message.reply_text(f"Your user ID is {context._user_id_and_data[0]}")
+
+
+def error(update: Update, context: CallbackContext):
+    logger.warning("Update %s caused error %s", update, context.error)
+
+def main():
+    """Start the bot."""
+
+    # Create Updater and pass it the bot's token
+    updater = Updater(TOKEN, use_context=True)
+
+    # Get dispatcher to register handlers
+
+    dp: Dispatcher = updater.dispatcher
+    
+    # Test Ping
+    dp.add_handler(CommandHandler("test", start))
+
+    # Test UID Ping
+    dp.add_handler(CommandHandler("uid", uid))
+
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the bot
+
+    updater.start_polling()
+
+    # Run bot until stopped
+    updater.idle()
+
