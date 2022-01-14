8b6c0cdcad8d20f53ba5d90745b9afcf5f94dd7c
Goutham Krishna <gauthamkrishna9991@live.com>
2022-01-14 20:52:45 +0530

    refactor: Sort imports + project vars

--- arxiv_telegram_bot/base.py
+++ arxiv_telegram_bot/base.py
@@ -4,48 +4,70 @@
 """
 # Arxiv Base Imports
 
-
+All Imports Necessary for Arxiv Bot 
 """
 
+# -- IMPORTS: LIBRARIES
+# - Standard Library Imnports
 import logging
-from setuptools import Command
+import os
 
+# - DotENV
+import dotenv
+
+# - Telegram Imports
+from telegram import Update
+
+# - Telegram Extended Imports
 from telegram.ext import (
     Updater,
     CommandHandler,
     Dispatcher,
-    MessageHandler,
-    Filters,
     CallbackContext,
 )
-from telegram import Update
-import os
 
-port = int(os.environ.get("PORT", 5000))
 
-# enable logging
+# -- SETUP
+
+# - Load DotENV
+dotenv.load_dotenv()
+
+# - Enable Logging
 logger = logging.getLogger(__name__)
 
-TOKEN = "5057319565:AAEX2dcT93FJ4AToOZlrVhvuPxJy051c_nQ"
 
-# Define a few command handlers
+# -- CONSTANTS
+
+# - Application Port
+PORT = int(os.environ.get("PORT", 5000))
+
+# - Application Token
+TOKEN = dotenv.get_key(".env", "TOKEN")
 
 
+# -- COMMAND HANDLERS
+
+# - 'start' Command Handler
 def start(update: Update, context: CallbackContext):
     """Send a message when the command 'start' is issued."""
     print(context._user_id_and_data[0])
     update.message.reply_text("Hi! Your userid")
 
 
+# - 'uid' Command Handler
 def uid(update: Update, context: CallbackContext):
     """Ping back the userid whose command created"""
-    update.message.reply_text(f"Your user ID is {context._user_id_and_data[0]}")
+    message_to_send = f"Your user ID is {context._user_id_and_data[0]}"
+    print(message_to_send)
+    update.message.reply_text(message_to_send)
 
 
+# - Error Handler
 def error(update: Update, context: CallbackContext):
     logger.warning("Update %s caused error %s", update, context.error)
 
 
+# -- MAIN FUNCTION
 def main():
     """Start the bot."""
 
