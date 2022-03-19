146353a833d03b47dc22c204a4ffabe7bc5350eb
Sonaal Pradeep <unofficial.sonaal@gmail.com>
2022-03-19 13:53:05 +0530

    chore: Remove commented lines of code

--- arxiv_telegram_bot/__main__.py
+++ arxiv_telegram_bot/__main__.py
@@ -7,25 +7,6 @@
 Main Module for Arxiv
 """
 
-"""
-# import arxiv
-
-# query_catalogues = ["cs.CV", "cs.LG", "cs.CL", "cs.AI", "cs.NE", "cs.RO"]
-# query_string = " OR ".join(query_catalogues)
-
-# search = arxiv.Search(
-#     query=query_string,
-#     max_results=100,
-#     sort_by=arxiv.SortCriterion.SubmittedDate,
-#     sort_order=arxiv.SortOrder.Descending,
-# )
-
-# for result in search.results():
-#     print(
-#         f"{result.title} - {result.published}\n{result.categories}\n{result.summary}\n\n"
-#     )
-"""
-
 from arxiv_telegram_bot.base import main
 
 if __name__ == "__main__":
