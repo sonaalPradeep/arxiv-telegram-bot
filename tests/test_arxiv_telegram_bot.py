00899d23222029d883f3b023340bd00370619f4f
Sonaal Pradeep <unofficial.sonaal@gmail.com>
2022-04-03 19:14:11 +0530

    fix : Black linting

--- tests/test_arxiv_telegram_bot.py
+++ tests/test_arxiv_telegram_bot.py
@@ -1,6 +1,7 @@
 from arxiv_telegram_bot import __version__
 import pytest
 
-@pytest.mark.skip(reason = "Auto-generated test by poetry")
+
+@pytest.mark.skip(reason="Auto-generated test by poetry")
 def test_version():
     assert __version__ == "0.1.0"
