9362511e2a74dfa8f975b85816384a354c493178
Sonaal Pradeep <unofficial.sonaal@gmail.com>
2022-04-03 18:58:38 +0530

    black files

--- tests/test_arxiv_telegram_bot.py
+++ tests/test_arxiv_telegram_bot.py
@@ -1,6 +1,7 @@
 from arxiv_telegram_bot import __version__
 import pytest
 
-@pytest.mark.skip(reason = "Auto-generated test by Poetry")
+
+@pytest.mark.skip(reason="Auto-generated test by Poetry")
 def test_version():
     assert __version__ == "0.1.1"
